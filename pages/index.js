08f099a72eaee53809dc825020fe62df32edde49
garrettjmu <garretthughes3@gmail.com>
2022-02-05 21:13:15 -0500

    add in ens avatar and name support

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
-import {DAppProvider} from "@usedapp/core";
+import {DAppProvider, useLookupAddress} from "@usedapp/core";
 import {ChakraProvider} from "@chakra-ui/react";
 import {Flex, Text, Button, Stack} from "@chakra-ui/react";
 import {useEthers} from "@usedapp/core";
+import { useEffect } from 'react';
+import Davatar from '@davatar/react'
 
 export default function Home() {
   const ConnectWalletButton = () => {
-    const {account, activateBrowserWallet} = useEthers();
+    const {account, activateBrowserWallet } = useEthers();
+    const ens = useLookupAddress()
 
     return (
         <Stack spacing={3} align='center'>
           {account ? (
+            <>
+              <Davatar
+                size={60}
+                address={account}
+                generatedAvatarType='jazzicon'
+              />
               <Text color='white'>
-                Hello, {account}!
+                Hello, {ens || account}!
               </Text>
+            </>
           ) : (
               <Button size='lg' onClick={()=>activateBrowserWallet()}>
                 Connect Wallet
